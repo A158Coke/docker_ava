02ac4f5c0d323d53a1cf15f10c355efe32de146f
Chen Yu <a1582536892@gmail.com>
2024-03-03 12:55:08 +0100

    comments

--- posgres/init.sh
+++ posgres/init.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
-
-# 启动 PostgreSQL 服务
 service postgresql start
-
-# 修改 pg_hba.conf 文件以取消 peer 认证方式并允许密码认证
 echo "host all all 0.0.0.0/0 trust" >> /etc/postgresql/14/main/pg_hba.conf
 echo "listen_addresses='*'" >> /etc/postgresql/14/main/postgresql.conf
 
-# 创建数据库和用户，并授予相应权限
+# Crea base de datos y usuarios y dar usuario el perimiso
 sudo -u postgres psql -c "CREATE DATABASE my_db;"
 sudo -u postgres psql -c "CREATE USER odoo WITH PASSWORD 'odoo';"
 sudo -u postgres psql -c "ALTER USER odoo WITH SUPERUSER;"
 sudo -u postgres psql -c "ALTER DATABASE my_db OWNER TO odoo;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE my_db TO odoo;"
 
-
-
-# 重新加载 PostgreSQL 配置
 sudo -u postgres psql -c "SELECT pg_reload_conf();"
-
-# 重启 PostgreSQL 服务
 service postgresql restart
 
-# 保持容器运行，以便查看日志
+# Mantenga el container running en el bacl
 tail -f /dev/null
